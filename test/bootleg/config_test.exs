91ab2549cbe300454f2cd7a35daecb61340ecaa8
Andrew Dixon <andrew.d.dixon@gmail.com>
2026-02-10 10:02:39 -0800

    Fix typo in very specific spec. (#319)
    * Fix typo in very specific spec.
    
    * Make the test less specific.
    
    ---------
    
    Co-authored-by: Andrew Dixon <dixon@largearcade.com>

--- test/bootleg/config_test.exs
+++ test/bootleg/config_test.exs
@@ -46,8 +46,8 @@ defmodule Bootleg.ConfigTest do
   test "use Bootleg.Config warns about deprecation" do
     assert capture_io(:stderr, fn ->
              use Bootleg.Config
-           end) ==
-             "\e[33mwarning: \e[0m`use Bootleg.Config` is deprecated; call `use Bootleg.DSL` instead.\n  test/bootleg/config_test.exs:48: Bootleg.ConfigTest.\"test use Bootleg.Config warns about deprecation\"/1\n\n"
+           end) =~
+             "`use Bootleg.Config` is deprecated; call `use Bootleg.DSL` instead."
   end
 
   test "get_role/1", %{local_user: local_user} do
